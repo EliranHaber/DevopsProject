e61b04213b56147d523e8a250b02560b5b2b6f71
Eliran Haber <jackylegstf2@gmail.com>
2025-09-13 14:23:12 +0300

    Fixed tomcat version

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 # Build script for Render deployment
 echo "Building HIT DevOps Project..."
 
-# Download and setup Tomcat
-wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz
-tar -xzf apache-tomcat-9.0.65.tar.gz
-mv apache-tomcat-9.0.65 tomcat
+# Download and setup Tomcat 8.5.100 (matching local setup)
+wget -q https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.100/bin/apache-tomcat-8.5.100.tar.gz
+tar -xzf apache-tomcat-8.5.100.tar.gz
+mv apache-tomcat-8.5.100 tomcat
 
 # Deploy our app to Tomcat
 mkdir -p tomcat/webapps/ROOT
 cp index.jsp tomcat/webapps/ROOT/
 cp -r WEB-INF tomcat/webapps/ROOT/
+
+# Make Tomcat executable
+chmod +x tomcat/bin/*.sh
 
 echo "Build completed successfully!"
