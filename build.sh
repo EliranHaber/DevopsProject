ecceaa12d5300dddd30067e323361efeec619c14
Eliran Haber <jackylegstf2@gmail.com>
2025-09-13 14:35:09 +0300

    Another attempt

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # Build script for Render deployment
 echo "Building HIT DevOps Project..."
 
-# Install Java 8
-apt-get update
-apt-get install -y openjdk-8-jdk
-
 # Download and setup Tomcat 8.5.100 (matching local setup)
 wget -q https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.100/bin/apache-tomcat-8.5.100.tar.gz
 tar -xzf apache-tomcat-8.5.100.tar.gz
@@ -19,11 +15,10 @@ cp -r WEB-INF tomcat/webapps/ROOT/
 # Make Tomcat executable
 chmod +x tomcat/bin/*.sh
 
-# Create startup script with Java environment
+# Create startup script (Render provides Java environment)
 echo '#!/bin/bash' > start.sh
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> start.sh
-echo 'export JRE_HOME=$JAVA_HOME/jre' >> start.sh
-echo 'export PATH=$JAVA_HOME/bin:$PATH' >> start.sh
+echo 'echo "Java version: $(java -version)"' >> start.sh
+echo 'echo "JAVA_HOME: $JAVA_HOME"' >> start.sh
 echo 'tomcat/bin/catalina.sh run' >> start.sh
 chmod +x start.sh
 
