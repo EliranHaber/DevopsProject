64bf9c179bc71b39ebe5dbf241b6fdce56edb613
Eliran Haber <jackylegstf2@gmail.com>
2025-09-13 14:12:49 +0300

    Add Tomcat to build for JSP support

--- build.sh
+++ build.sh
@@ -2,16 +2,14 @@
 # Build script for Render deployment
 echo "Building HIT DevOps Project..."
 
-# Create target directory
-mkdir -p target
+# Download and setup Tomcat
+wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz
+tar -xzf apache-tomcat-9.0.65.tar.gz
+mv apache-tomcat-9.0.65 tomcat
 
-# Copy JSP files to target
-cp index.jsp target/
-cp -r WEB-INF target/
-
-# Create a simple JAR structure (Render expects JAR)
-cd target
-jar cf hit-devops-project.jar *
-cd ..
+# Deploy our app to Tomcat
+mkdir -p tomcat/webapps/ROOT
+cp index.jsp tomcat/webapps/ROOT/
+cp -r WEB-INF tomcat/webapps/ROOT/
 
 echo "Build completed successfully!"
